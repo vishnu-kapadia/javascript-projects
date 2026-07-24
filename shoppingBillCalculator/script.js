1180776ce540200fdb68c9001afb58bb8a813e03
vishnu-kapadia <vishnukapadiatech@gmail.com>
2026-07-24 21:45:00 +0530

    Add typeof checks and additional calculations to billing

--- shoppingBillCalculator/script.js
+++ shoppingBillCalculator/script.js
@@ -1,4 +1,6 @@
 let customerName = "Rahul";
+let customerAge = 22;
+let isMember = true;
 
 let notebookPrice = 80;
 let penPrice = 20;
@@ -15,19 +17,50 @@ let bagTotal = bagPrice * bagQuantity;
 let totalBill = notebookTotal + penTotal + bagTotal;
 
 let discount = 100;
-
 let finalBill = totalBill - discount;
 
-let gst = finalBill * 18 / 100;
+let gstRate = 18;
+let gst = finalBill * gstRate / 100;
 
 let amountToPay = finalBill + gst;
 
+let totalItems = notebookQuantity + penQuantity + bagQuantity;
+let averageItemPrice = totalBill / totalItems;
+let remainingAmount = amountToPay % 100;
+let doubleDiscount = discount * 2;
+let increasedBagPrice = bagPrice + 50;
+
+let customerType = "Regular";
+let rewardPoints = 150;
+let hasCoupon = false;
+
 console.log("Customer Name:", customerName);
+console.log("Customer Age:", customerAge);
+console.log("Is Member:", isMember);
+
 console.log("Notebook Total:", notebookTotal);
 console.log("Pen Total:", penTotal);
 console.log("Bag Total:", bagTotal);
+
 console.log("Total Bill:", totalBill);
 console.log("Discount:", discount);
 console.log("Bill After Discount:", finalBill);
-console.log("GST:", gst);
+
+console.log("GST Rate:", gstRate + "%");
+console.log("GST Amount:", gst);
 console.log("Final Amount:", amountToPay);
+
+console.log("Total Items Purchased:", totalItems);
+console.log("Average Price Per Item:", averageItemPrice);
+console.log("Remaining Amount (Modulo 100):", remainingAmount);
+console.log("Double Discount:", doubleDiscount);
+console.log("Increased Bag Price:", increasedBagPrice);
+
+console.log("Customer Type:", customerType);
+console.log("Reward Points:", rewardPoints);
+console.log("Has Coupon:", hasCoupon);
+
+console.log("Type of customerName:", typeof customerName);
+console.log("Type of customerAge:", typeof customerAge);
+console.log("Type of isMember:", typeof isMember);
+console.log("Type of totalBill:", typeof totalBill);
